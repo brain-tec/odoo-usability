c66289c8d1028782136f6e6d4cd845c95303cc24
Kevin Khao <kevinkhao@gmail.com>
2021-01-18 13:41:24 +0100

    [14.0][ADD] google_calendar_hide_credentials

---
+++ google_calendar_hide_credentials/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Google Calendar Hide Credentials",
+    "version": "14.0.1.0.0",
+    "category": "Productivity",
+    "description": "",
+    "depends": ["google_calendar"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
