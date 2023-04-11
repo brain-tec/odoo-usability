6c561f4314cce9e052137d64730e9ac22e9c1a1e
David Beal <david.beal@akretion.com>
2023-04-11 16:57:13 +0200

    FIX geo called module

---
+++ base_geoengine/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Fake",
+    "version": "16.0.1.0.0",
+    "author": "Akretion",
+    "development_status": "Mature",
+    "category": "Uncategorized",
+    "website": "https://akretion.com",
+    "license": "AGPL-3",
+    "depends": [
+    ],
+}
