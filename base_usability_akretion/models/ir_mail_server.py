62ca33ed4b0b14e14bb37bb9c665057e7ecca331
Bonnerue <thomas.bonnerue@akretion.com>
2025-01-09 12:02:40 +0100

    [FIX] base_usability_akretion ir.mail_server send mail

--- base_usability_akretion/models/ir_mail_server.py
+++ base_usability_akretion/models/ir_mail_server.py
@@ -18,6 +18,13 @@ class IrMailServer(models.Model):
             smtp_ssl_certificate=None, smtp_ssl_private_key=None,
             smtp_debug=False, smtp_session=None):
         # Start copy from native method
+        smtp = smtp_session
+        if not smtp:
+            smtp = self.connect(
+                smtp_server, smtp_port, smtp_user, smtp_password, smtp_encryption,
+                smtp_from=message['From'], ssl_certificate=smtp_ssl_certificate, ssl_private_key=smtp_ssl_private_key,
+                smtp_debug=smtp_debug, mail_server_id=mail_server_id,)
+
         smtp_from, smtp_to_list, message = self._prepare_email_message(
             message, smtp)
         # End copy from native method
