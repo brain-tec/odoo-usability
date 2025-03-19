8a7475a761a31822bd2795da99418c2540598ab0
Alexis de Lattre <alexis.delattre@akretion.com>
2025-03-19 13:58:47 +0000

    [IMP] base_usability: monkey patch formatLang

---
+++ base_usability/models/misc.py
@@ -0,0 +1,39 @@
+# Copyright 2025 Akretion France (https://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
+from odoo.tools import misc
+from openerp.tools import float_compare
+
+
+class BaseUsabilityInstalled(models.AbstractModel):
+    _name = "base.usability.installed"
+    _description = "Base Usability Installed"
+
+
+formatLang_original = misc.formatLang
+
+
+def formatLang(
+        self, value, digits=None, grouping=True,
+        monetary=False, dp=False, currency_obj=False, int_no_digits=True):
+    with api.Environment.manage():
+        env = api.Environment(self.cr, self.uid, {})
+        if (
+                'base.usability.installed' in env and
+                int_no_digits and
+                not monetary and
+                isinstance(value, float) and
+                dp):
+            prec = env['decimal.precision'].precision_get(dp)
+            if not float_compare(value, int(value), precision_digits=prec):
+                digits = 0
+                dp = False
+    res = formatLang_original(
+        self, value, digits=digits, grouping=grouping,
+        monetary=monetary, dp=dp, currency_obj=currency_obj)
+    return res
+
+
+misc.formatLang = formatLang
