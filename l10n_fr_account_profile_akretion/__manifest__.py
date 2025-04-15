b9230b2cf55b75f236e50c2ad75a186d35276ed0
Alexis de Lattre <alexis.delattre@akretion.com>
2025-04-15 17:52:51 +0200

    Add modules base_profile_akretion and l10n_fr_account_profile_akretion

---
+++ l10n_fr_account_profile_akretion/__manifest__.py
@@ -0,0 +1,96 @@
+# Copyright 2025 Akretion France (https://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Profile for France',
+    'version': '16.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Module set for accounting for a French company',
+    'author': 'Akretion',
+    'website': 'https://github.com/akretion/odoo-usability',
+    'depends': [
+        ### MISC
+        'account_usability_akretion',  # akretion/odoo-usability
+        'account_usability',  # OCA/account-financial-tools
+        'l10n_fr_siret_lookup',  # OCA/l10n-france
+        'account_payment_partner',  # OCA/bank-payment
+        'account_lock',  # addons officiels
+        'account_move_name_sequence',  # OCA/account-financial-tools
+        'account_move_csv_import',  # akretion/account-move-import
+        'account_move_line_reconcile_manual',  # OCA/account-reconcile
+        ### INVOICING
+        'account_invoice_fiscal_position_update',  # OCA/account-invoicing
+        'account_fiscal_position_vat_check',  # OCA/account-financial-tools
+        'account_invoice_facturx',  # OCA/edi
+        ### FINANCIAL REPORTS
+        'account_financial_report',  # OCA/account-financial-reporting
+        'account_balance_ebp_csv_export',  # OCA/l10n-france
+        'l10n_fr_mis_reports',  # OCA/l10n-france
+        'l10n_fr_fec_oca',  # OCA/l10n-france
+        ### BANK STATEMENTS
+        'account_statement_completion_label_simple',  # akretion/bank-statement-reconcile-simple
+        #'account_statement_completion_label_simple_sale',  # akretion/bank-statement-reconcile-simple
+        'account_statement_import_file_reconcile_oca',  # OCA/bank-statement-import
+        'account_statement_import_ofx',  # OCA/bank-statement-import
+        'account_statement_import_fr_cfonb',  # OCA/l10n-france
+        'account_reconcile_oca',  # OCA/account-reconcile
+        ### CURRENCY RATES
+        'currency_rate_update',  # OCA/currency
+        'currency_old_rate_notify',  # OCA/currency
+        ### INVOICE IMPORT
+        'account_invoice_import_simple_pdf',  # OCA/edi
+        'account_invoice_import_facturx',  # OCA/edi
+        'l10n_fr_account_invoice_import_facturx',  # OCA/l10n-france
+        ### OVERDUE
+        'account_invoice_overdue_warn',  # OCA/credit-control
+        #'account_invoice_overdue_warn_sale',  # OCA/credit-control
+        'account_invoice_overdue_reminder',  # OCA/credit-control
+        ### FRENCH DECLARATIONS
+        'l10n_fr_account_vat_return_teledec',  # OCA/l10n-france
+        'l10n_fr_account_vat_return_einvoice_generate',  # OCA/l10n-france
+        # Warning: intrastat_product depends on sale_stock and purchase_stock !
+        'l10n_fr_intrastat_product',  # OCA/l10n-france
+        'product_net_weight',  # OCA/product-attribute
+        'l10n_fr_intrastat_service',  # OCA/l10n-france
+        'l10n_fr_das2',  # OCA/l10n-france
+        ### INVOICE IMPORT
+        #'account_invoice_download_ovh',  # OCA/edi
+        #'account_invoice_download_scaleway',  # OCA/edi
+        ### PAYMENT ORDERS and DEBIT ORDERS
+        # It is recommended to use the code of the following PRs that work together:
+        # https://github.com/OCA/bank-payment/pull/1174 (all modules of OCA/bank-payment)
+        # https://github.com/OCA/l10n-france/pull/560 (account_banking_fr_lcr)
+        # https://github.com/OCA/l10n-france/pull/490 (l10n_fr_account_banking_pain_base)
+        #'partner_bank_acc_type_constraint',  # OCA/partner-contact
+        #'account_banking_sepa_credit_transfer',  # OCA/bank-payment
+        #'account_banking_sepa_direct_debit',  # OCA/bank-payment
+        #'account_banking_fr_lcr',  # OCA/l10n-france
+        #'l10n_fr_account_banking_pain_base',  # OCA/l10n-france
+        ### PY3O
+        #'account_invoice_facturx_py3o',  # OCA/edi
+        ### CHORUS
+        #'l10n_fr_chorus_account',  # OCA/l10n-france
+        #'l10n_fr_chorus_sale',  # OCA/l10n-france
+        #'l10n_fr_chorus_facturx',  # OCA/l10n-france
+        ### CUTOFF
+        #'account_cutoff_start_end_dates',  # OCA/account-closing
+        #'account_cutoff_picking',  # OCA/account-closing
+        #'account_cutoff_accrual_subscription',  # OCA/account-closing
+        ### MISC
+        #'account_check_deposit',  # OCA/account-financial-tools
+        #'account_cash_deposit',  # OCA/account-financial-tools
+        #'account_invoice_pricelist',  # OCA/account-invoicing
+        #'account_asset_management',  # OCA/account-financial-tools
+        ### MOONCARD
+        #'mooncard_payment_card',  # akretion/odoo-mooncard-connector
+        #'l10n_fr_base_newgen_payment_card',  # akretion/odoo-mooncard-connector
+        #'base_newgen_payment_card_start_end_dates',  # akretion/odoo-mooncard-connector
+        ],
+    'excludes': [
+        'l10n_fr_fec',
+        'account_edi_ubl_cii',
+        ],
+    'installable': True,
+}
