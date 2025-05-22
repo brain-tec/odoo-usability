03d3f30df6c64ecb84625a7ff4c0b0038f6f837c
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-22 16:12:31 +0200

    [MIG] product_category_tax from v14 to v16

--- product_category_tax/__manifest__.py
+++ product_category_tax/__manifest__.py
@@ -1,17 +1,17 @@
-# Copyright 2015-2020 Akretion (http://www.akretion.com)
+# Copyright 2015-2025 Akretion France (https://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Category Tax',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Adds sale and purchase taxes on product category',
     'description': "",
     'author': 'Akretion',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/akretion/odoo-usability',
     'depends': ['account'],
     'data': ['product_view.xml'],
-    'installable': False,
+    'installable': True,
 }
