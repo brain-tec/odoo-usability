faea568a6dad6518d4d2c488188c410c44d09478
Florian da Costa <florian.dacosta@akretion.com>
2025-09-02 16:02:40 +0200

    [MIG] commission_simple_agent : technical migration to 18

--- commission_simple_agent/__manifest__.py
+++ commission_simple_agent/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Commission Simple Agent',
-    'version': '16.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Glue module between commission_simple and sale_agent',
