dfdd09260d6ef117e99bc9d113a84e7b748135f5
Alexis de Lattre <alexis.delattre@akretion.com>
2025-09-02 10:33:00 +0000

    [IMP] commission_simple: XSLX report sorted by date (inheritable)

--- commission_simple/reports/commission_result_xlsx.py
+++ commission_simple/reports/commission_result_xlsx.py
@@ -53,10 +53,13 @@ class CommissionResultXlsx(models.AbstractModel):
             sheet.write(i, col_vals['pos'], col_vals['label'], styles['col_title'])
             sheet.set_column(col_vals['pos'], col_vals['pos'], col_vals['width'])
         # table content
-        for line in result.line_ids:
+        for line in result.line_ids.sorted(key=self._prepare_sort_field()):
             i += 1
             for col_key, value in line._prepare_commission_xlsx().items():
                 sheet.write(i, coldict[col_key]["pos"], value, styles[coldict[col_key]["style"]])
+
+    def _prepare_sort_field(self):
+        return "date"
 
     def _prepare_xlsx_cols(self):
         cols = [  # key, label, width, style_suffix
