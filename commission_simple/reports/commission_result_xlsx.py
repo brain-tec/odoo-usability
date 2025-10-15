f12218c4a04648da763febfe391fefa72cb1f923
Alexis de Lattre <alexis.delattre@akretion.com>
2025-10-15 09:31:57 +0200

    [FIX] commission_simple_*: backport from 16 to 14

--- commission_simple/reports/commission_result_xlsx.py
+++ commission_simple/reports/commission_result_xlsx.py
@@ -2,7 +2,7 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, tools, Command, _
+from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError
 from datetime import datetime
 from odoo.tools.misc import format_datetime
